ef6a31f54c52115d069b1c08172d7255f52d485a
David Irwin <daveirwin1@gmail.com>
2019-02-21 21:26:05 -0500

    Updated copyright

--- fidoserver/fidoserverInstall/src/certimport.sh
+++ fidoserver/fidoserverInstall/src/certimport.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
 ###############################################################
 #
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License, as published by the Free Software Foundation and
-# available at http://www.fsf.org/licensing/licenses/lgpl.html,
-# version 2.1 or above.
+# Copyright StrongAuth, Inc. All Rights Reserved.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
-#
-# Copyright (c) 2001-2018 StrongAuth, Inc.
-#
-# $Date$
-# $Revision$
-# $Author$
-# $URL$
+# Use of this source code is governed by the Gnu Lesser General Public License 2.3.
+# The license can be found at https://github.com/StrongKey/FIDO-Server/LICENSE
 #
 ###############################################################
 

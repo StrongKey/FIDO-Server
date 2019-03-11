15be9f344806a4851ca0c968f3812c41f6e24a23
Pushkar Marathe <pushkar.marathe@strongkey.com>
2019-03-11 09:45:09 -0700

    adding process for ubuntu and debian install

---
+++ fidoserver/fidoserverInstall/cleanup-ubuntu.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+###################################################################################
+# Copyright StrongAuth, Inc. All Rights Reserved.
+#
+# Use of this source code is governed by the Gnu Lesser General Public License 2.3.
+# The license can be found at https://github.com/StrongKey/FIDO-Server/LICENSE
+###################################################################################
+
+LOGNAME=/root/strongkey_logs/cleanup-saka-$(date +%s)
+IFS="
+"
+
+if ! [ -d /root/strongkey_logs ]; then
+        mkdir /root/strongkey_logs
+fi
+
+echo "Stopping SKFS services..." | tee -a $LOGNAME
+service mysqld restart 
+service mysqld stop 
+service glassfishd stop 
+
+echo "Restoring original system files..." | tee -a $LOGNAME
+sed -i '/skfsrc/d' /etc/bash.bashrc
+cp /etc/org/sudoers /etc
+
+echo "Removing SKFS configuration files..." | tee -a $LOGNAME
+/lib/systemd/systemd-sysv-install disable mysqld
+/lib/systemd/systemd-sysv-install disable glassfishd
+
+rm /etc/my.cnf
+rm /etc/skfsrc
+rm /etc/init.d/mysqld
+rm /etc/init.d/glassfishd
+
+echo "Removing User..." | tee -a $LOGNAME
+userdel -r strongkey
+
+if $(id strongkey &> /dev/null); then
+	echo -e "\E[31m'strongkey' user not fully removed. Kill all processes owned by 'strongkey' and try again." | tee -a $LOGNAME
+        tput sgr0
+        exit 1
+fi
+
+echo "Done!" | tee -a $LOGNAME
+
+exit 0
